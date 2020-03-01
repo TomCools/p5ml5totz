fd05637ae5b3d2dc64c707dc693c68e26bbe3f52
Tom Cools <tom.cools@live.be>
2020-03-01 14:30:57 +0100

    Removed demo examples

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
+// Images and game objects
 let bgImg;
 let totzImg;
 let totz;
 let floor;
+let state = 'Close';
 
+// Forces
 let gravityForce;
 let jumpForce;
 
+// Machine learning
+let classifier;
+// Model URL (Change this URL with your own model if you want it trained for something else than my plushy :-)
+let imageModelURL = 'https://teachablemachine.withgoogle.com/models/HRoMDshS/';
+
 function preload() {
+    // Load the model :-)
+    classifier = ml5.imageClassifier(imageModelURL + 'model.json', modelLoaded);
+
     bgImg = loadImage('assets/background.png');
     totzImg = loadImage('assets/totzSprite.png');
 }
@@ -19,10 +30,14 @@ function setup() {
 
     floor = new Floor(height - 30);
 
+    // Create the video
+    video = createCapture(VIDEO);
+    video.size(320, 240);
+    video.hide()
+
     // Gravity is a downward movement, so the horizontal force is 0.
     // The vertical force is 0.05, meaning down.
     gravityForce = new createVector(0, 0.05);
-
     // Jumping will be a force vector as well! :-)
     jumpForce = new createVector(0, -3);
 }
@@ -45,7 +60,9 @@ function draw() {
     }
 
     totz.display();
-    floor.display();
+
+    // Not drawing the floor, the background image already looks like a floor :-)
+    //floor.display();
 }
 
 function jump() {
@@ -54,5 +71,38 @@ function jump() {
 
 // This method gets invoked by P5.js on touch / click
 function touchStarted() {
-    jump();
+    this.jump();
+}
+
+// As loading the model can take a while, we'll use a callback function.
+// Once the model is loaded, we'll start classifying every 100ms.
+function modelLoaded() {
+    console.log('Model Loaded!');
+    setInterval(function(){classifyVideo()},100);
+}
+
+// Get a prediction for the current video frame
+function classifyVideo() {
+    flippedVideo = ml5.flipImage(video)
+    classifier.classify(flippedVideo, gotResult);
+}
+
+// When we get a result
+function gotResult(error, results) {
+    // If there is an error
+    if (error) {
+        console.error(error);
+        return;
+    }
+    // The results are in an array ordered by confidence, so [0] contains bet result!
+    let newState = results[0].label;
+    console.log(newState);
+
+    // We consider it a jump when the state goes from
+    // arms Open to arms Closed :-)
+    if(state === "Open" && newState === "Close") {
+        // trigger a jump
+        this.jump();
+    }
+    state = newState;
 }
